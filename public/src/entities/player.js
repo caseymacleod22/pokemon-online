8e2d762e62721b4a57f8481e7951ade764cf6fe3
Casey MacLeod <caseymacleod22@gmail.com>
2021-08-05 00:28:55 -0400

    add new player.png

--- public/src/entities/player.js
+++ public/src/entities/player.js
@@ -1,6 +1,12 @@
 class player {
   constructor() {
-    this.properties = [];
+   this.pos = new position(this, 300, 300)
+   this.img = new graphics(this, this.pos, 'boy_run')
+
+    this.properties = [
+      this.pos,
+      this.img
+    ];
 
     objects.push(this);
   }
--- public/src/main.js
+++ public/src/main.js
@@ -3,6 +3,9 @@ client.init("localhost:3000");
 
 function setup() {
   Screen.init();
+  new.rawImage('boy_run')
+
+  new.player()
 }
 function step() {
 }
