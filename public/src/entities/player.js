3fb36aaf09d4c1e75119ca0acd540c715bf6f659
Casey MacLeod <caseymacleod22@gmail.com>
2021-08-07 23:32:05 -0400

    player class

--- public/src/entities/player.js
+++ public/src/entities/player.js
@@ -3,10 +3,10 @@ class player {
    this.pos = new position(this, 300, 300)
    this.img = new graphics(this, this.pos, 'boy_run')
 
-    this.properties = [
-      this.pos,
-      this.img
-    ];
+    // this.properties = [
+    //   this.pos,
+    //   this.img
+    // ];
 
     objects.push(this);
   }
