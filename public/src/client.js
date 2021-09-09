c2574ff13ecba598ccd100cea96f824b5515b27e
Casey MacLeod <caseymacleod22@gmail.com>
2021-09-09 18:08:29 -0400

    client adjustments

--- public/src/client.js
+++ public/src/client.js
@@ -1,10 +1,10 @@
-var client = {
-  socket: null,
-  init: function(link) {
-    client.socket = io.connect(link);
-  },
-  send: function(data, key) {
-    if(!key) key = "send";
-    client.socket.emit(key, data);
-  }
-}
+// var client = {
+//   socket: null,
+//   init: function(link) {
+//     client.socket = io.connect(link);
+//   },
+//   send: function(data, key) {
+//     if(!key) key = "send";
+//     client.socket.emit(key, data);
+//   }
+// }
